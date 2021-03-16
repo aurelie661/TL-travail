0c4c9f67b56f14dbeb8b65fd06cf0ee219623ca9
aurelie661 <78204795+aurelie661@users.noreply.github.com>
2021-03-16 11:23:18 +0100

    Delete lecture_fichier_txt.php

--- lecture_fichier_txt.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$myfile=fopen("C://Users/Liyah/Documents/php/client.txt", "r");
-$content=fread($myfile,filesize("C://Users/Liyah/Documents/php/client.txt"));
-echo $content;
-fclose($myfile);
