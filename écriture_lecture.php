acff56adf6ecfcc4d9e23699adc3cb1f904bf87d
aurelie661 <boutaurel59@gmail.com>
2021-03-16 11:01:59 +0100

    exo_fichier

--- écriture_lecture.php
+++ écriture_lecture.php
@@ -1,12 +1,13 @@
 <?php
 
 $myFile=fopen("C://Users/Liyah/Documents/php/client2.txt", "a");
-for($i=0;$i<3;$i++){
+for($i=0;$i<2;$i++){
     $nom=readline("Nom :");
     $prenom=readline("Prénom :");
     $numTel=readline("Numéro de téléphone :");
     $wR=fwrite($myFile,"Nom : ".$nom.", Prénom : ".$prenom.", Numéro de téléphone : ".$numTel."\n");
 }
+fclose($myFile);
 $myFile=fopen("C://Users/Liyah/Documents/php/client2.txt", "r");
 $content=fread($myFile, filesize("C://Users/Liyah/Documents/php/client2.txt"));
 echo $content;
